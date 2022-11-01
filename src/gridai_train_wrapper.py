ec2afdac389b4ef78b73f08ea6dba7bf82065c7e
Jakub Mroczek <Jakub.Mroczek@tomtom.com>
2022-11-01 11:21:10 +0100

    wrapper for grid-ai runs to fix CLI problems

---
+++ src/gridai_train_wrapper.py
@@ -0,0 +1,15 @@
+import sys, os
+
+if __name__ == '__main__':
+    EXPERIMENT_PREFIX = '--experiment='
+    
+    # Input format: --experiment=example from STDOUT
+    # We handle only 1 experiment at the time
+    assert len(sys.argv) == 2
+    assert EXPERIMENT_PREFIX in sys.argv[1]
+
+    experiment_arg = sys.argv[1]
+    experiment_name = experiment_arg[len(EXPERIMENT_PREFIX):]
+    
+    print(f"Executing: train.py experiment={experiment_name}")
+    os.system(f"python3 src/train.py experiment={experiment_name}")
