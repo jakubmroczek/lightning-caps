fa449400b71ae440143e9a9c91fb4449bdd8ceb4
Jakub Mroczek <Jakub.Mroczek@tomtom.com>
2022-11-01 12:53:42 +0100

    fix wrapper script

--- src/gridai_train_wrapper.py
+++ src/gridai_train_wrapper.py
@@ -1,15 +1,14 @@
 import sys, os
 
 if __name__ == '__main__':
-    EXPERIMENT_PREFIX = '--experiment='
+    EXPERIMENT_PREFIX = '--experiment'
     
     # Input format: --experiment=example from STDOUT
     # We handle only 1 experiment at the time
-    assert len(sys.argv) == 2
+    assert len(sys.argv) == 3
     assert EXPERIMENT_PREFIX in sys.argv[1]
 
-    experiment_arg = sys.argv[1]
-    experiment_name = experiment_arg[len(EXPERIMENT_PREFIX):]
-    
+    experiment_name = sys.argv[2]
+
     print(f"Executing: train.py experiment={experiment_name}")
     os.system(f"python3 src/train.py experiment={experiment_name}")
